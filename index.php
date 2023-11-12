9a3454514cd4c1d0c252e28f8e3bab603cd3db2f
Anaxinomus <89991190+Anaxinomus@users.noreply.github.com>
2023-11-13 00:06:52 +0300

    Update and rename license_check.php to  index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lisans Kontrol</title>
+</head>
+<body>
+
 <?php
 // Lisans anahtarını post parametresinden al
 $licenseKey = $_POST['licenseKey'];
@@ -9,3 +18,6 @@ $validLicense = ($licenseKey == "exampleKey");
 // Sonucu JSON formatında döndür
 echo json_encode(['valid' => $validLicense]);
 ?>
+
+</body>
+</html>
